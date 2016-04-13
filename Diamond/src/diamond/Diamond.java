4afa708046d7195eb57b22718454328465c1f624
mjqi80 <mjqi89@gmail.com>
2016-04-13 21:06:13 +0200

    Diamond uploaded
    upload the project "Diamond"

---
+++ Diamond/src/diamond/Diamond.java
@@ -0,0 +1,36 @@
+package diamond;
+
+public class Diamond {
+
+    public static void main(String[] args) {
+        new Diamond().diamond_Print();
+    }
+    
+    public void diamond_Print() {
+        int i; // rows
+        int j; // cols        
+
+	for (j=1; j<=5; j++)
+	{
+            for (i=1; i<=8-j; i++)
+                System.out.print(" ");
+
+            for (i=1; i<=2*j-1; i++)
+               System.out.print("*");
+
+               System.out.print("\n");
+	}
+
+	for (j=6; j>=1; j--)
+	{
+	    for (i=1; i<=8-j; i++)
+		System.out.print(" ");
+
+            for (i=1; i<=2*j-1; i++)
+                System.out.print("*");
+
+                System.out.print("\n");
+	}
+    }
+    
+}
